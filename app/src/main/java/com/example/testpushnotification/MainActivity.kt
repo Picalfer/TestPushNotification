60ad6d41d7c319f37afae127ed74c20416dfdae0
Arthur Kozhemyakin <trable228@icloud.com>
2023-07-12 23:38:20 +0300

    pushes with payload

--- app/src/main/java/com/example/testpushnotification/MainActivity.kt
+++ app/src/main/java/com/example/testpushnotification/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.example.testpushnotification
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.testpushnotification.notification.PushService
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var pushBroadcastReceiver: BroadcastReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        pushBroadcastReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                val extras = intent?.extras
+                Log.d("TEST", "Message received")
+                extras?.keySet()?.firstOrNull { it == PushService.KEY_ACTION }?.let { key ->
+                    Log.d("TEST", "Action key -> $key")
+                    when (extras.getString(key)) {
+                        PushService.ACTION_SHOW_MESSAGE -> {
+                            extras.getString(PushService.KEY_MESSAGE)?.let { message ->
+                                Log.d("TEST", "Message key -> $message")
+                                Toast.makeText(
+                                    applicationContext,
+                                    message,
+                                    Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+
+                        else -> Log.e("TEST", "No needed key found")
+                    }
+                }
+            }
+        }
+
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(PushService.INTENT_FILTER)
+
+        registerReceiver(pushBroadcastReceiver, intentFilter)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(pushBroadcastReceiver)
     }
 }
--- app/src/main/java/com/example/testpushnotification/notification/PushService.kt
+++ app/src/main/java/com/example/testpushnotification/notification/PushService.kt
@@ -1,12 +1,34 @@
 package com.example.testpushnotification.notification
 
+import android.content.Intent
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class PushService : FirebaseMessagingService() {
 
     override fun onNewToken(newToken: String) {
         super.onNewToken(newToken)
         Log.d("TEST", "token: $newToken")
+
+        // send registration token to server
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        val intent = Intent(INTENT_FILTER)
+        message.data.forEach {entity ->
+            intent.putExtra(entity.key, entity.value)
+        }
+
+        sendBroadcast(intent)
+    }
+
+    companion object {
+        const val INTENT_FILTER = "PUSH_EVENT"
+        const val KEY_ACTION = "action"
+        const val KEY_MESSAGE = "message"
+
+        const val ACTION_SHOW_MESSAGE = "show_message"
     }
 }
